fddc49732d6f1d8a1e83082859c15818b6460a39
Sean Greth <sgreth@uncc.edu>
2017-10-11 22:09:03 +0000

    Done with section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,15 @@ class MoviesController < ApplicationController
     end
     
     def show
-        id = params[:id] #retrieve movie ID from URI route
-        @movie = Movie.find(id) #look up movie by unique ID
-        #will render app/views/movies/show.html.haml by default
-        
+        # id = params[:id] #retrieve movie ID from URI route
+        # @movie = Movie.find(id) #look up movie by unique ID
+        # #will render app/views/movies/show.html.haml by default
+        # byebug
+        @movie = Movie.where(:id => params[:id]).first
+        # logger.debug(:id)
+        # logger.debug(params[:id])
+        # logger.debug(@movie)
+        # raise params.inspect
         
     end
     
