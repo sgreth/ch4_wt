0f5083bc7a630c9f9a4f1db1f4b7a896ec393ec5
Sean Greth <sgreth@uncc.edu>
2017-10-11 22:12:33 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,6 +16,11 @@ class MoviesController < ApplicationController
         
     end
     
+    def new
+        @movie = Movie.new
+        #default: render 'new' template
+        
+    end
     
     # add below all other methods
     private
